adb4f78a02e55bd51002cdc2775e87525f846b56
huahu <action0324@gmail.com>
2013-08-07 20:00:16 +0800

    Change runs.py

--- runs.py
+++ runs.py
@@ -1,6 +1,6 @@
 import tornado.ioloop
 from application import application
-post = 8088
+port = 8088
 if __name__ == "__main__":
-    application.listen(post)
+    application.listen(port)
     tornado.ioloop.IOLoop.instance().start()
